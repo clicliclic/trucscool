0a7134d33ef7b3937f05cc3eaf3f01e097452f7c
Hgo0123 <hcordillot@gmail.com>
2023-05-19 14:23:43 +0200

    delete name on image

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -4,7 +4,6 @@ const Image = (props) => {
       <>
         <div className='image_ctn lg:basis-1/3 basis-1/2 lg:py-6 py-2 relative' onClick={() => props.openModal(props.index)}>
             <img id={props.index} src={props.src} alt={props.name} />
-            <p className='font-secondary text-white lg:text-xl text-lg absolute lg:bottom-8 bottom-2 left-2/4 -translate-x-2/4'>{props.name}</p>
         </div>
       </>
     )
