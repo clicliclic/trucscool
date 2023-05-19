852d6e43c869e75941d40732ce27e8c95f73058d
Hgo0123 <hcordillot@gmail.com>
2023-05-19 02:11:24 +0200

    fix border on modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -2,7 +2,7 @@ const Modal = (props) => {
 
     return (
       <>
-        <div className='h-screen w-screen border absolute top-0'>
+        <div className='h-screen w-screen absolute top-0'>
             <p className='text-black text-lg py-4 pl-6 font-main' onClick={props.handleClick}>&lt;-</p>
             <img src={props.src} alt="" />
         </div>
